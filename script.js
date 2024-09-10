c1ebeab73f3f62462a5e7e285477eaa954a2ce3f
AzkaKhizer <144557215+AzkaKhizer@users.noreply.github.com>
2024-09-10 20:19:27 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var nameElement = document.getElementById("name");
+var contactElement = document.getElementById("contact");
+if (nameElement && contactElement) {
+    nameElement.innerText = "Azka Khizer";
+    contactElement.innerText = "Contact: azka.khizer@gmail.com | +921234567";
+}
---
+++ script.ts
@@ -0,0 +1,7 @@
+const nameElement = document.getElementById("name");
+const contactElement = document.getElementById("contact");
+
+if (nameElement && contactElement) {
+  nameElement.innerText = "Azka Khizer"; 
+  contactElement.innerText = "Contact: azka.khizer@gmail.com | +921234567"; 
+}
